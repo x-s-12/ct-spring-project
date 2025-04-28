b2019cc281224c54ae9bc4f999565c81f43f9a0a
xs-12 <1428761936@qq.com>
2025-04-28 17:30:30 +0800

    first add

---
+++ src/main/java/com/ct/CtSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtSpringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CtSpringProjectApplication.class, args);
+    }
+
+}
